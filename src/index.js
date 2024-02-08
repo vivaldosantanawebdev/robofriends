894ff2579779158badeaaf2c0b5cfc5ebfd69dc2
Vivaldo Santana <vivaldo.santana.ca@gmail.com>
2024-02-08 00:22:52 -0500

    Project Done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
+import App from './App';
 import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 import {robots} from './robots';
@@ -10,11 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <h1>RoboFriends</h1>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-     
+      <App robots ={robots} />
     </div>
     
   </React.StrictMode>
